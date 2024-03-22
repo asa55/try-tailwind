aac5ce6fddfb9b4091efbacbae332a9604a4eea2
asa55 <asa55@pitt.edu>
2024-03-22 00:19:10 +0000

    try grid and flexbox layout

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div class="grid grid-cols-98 h-screen">
+        <div class="col-span-9 bg-red-400 border-2 flex items-center justify-center">01</div>
+        <div class="row-span-12 bg-yellow-400 border-2 flex items-center justify-center">02</div>
+        <div class="row-span-12 col-span-2 bg-green-400 border-2 flex items-center justify-center">03</div>
+        <div class="row-span-12 col-span-6 bg-blue-400 border-2 flex items-center justify-center">04</div>
+        <div class="col-span-9 bg-purple-400 border-2 flex items-center justify-center">05</div>
+      </div>
+    </>
   )
 }
 
